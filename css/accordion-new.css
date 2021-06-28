d2e09f86233a9b8b13a2bde2681df37f19ab424a
Julie <85100500+julie-konanchuk@users.noreply.github.com>
2021-06-28 14:06:13 +0300

    initiate code

---
+++ css/accordion-new.css
@@ -0,0 +1,48 @@
+@import url('https://fonts.googleapis.com/css2?family=Prata&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap');
+
+body {
+    background: #261e1c;
+    width: 450px;
+}
+.accordion-wrapper{
+    align-items: flex-start;
+    height: 100vh;
+    min-width: 600px;
+}
+.accordion-item-title{
+    width: 100%;
+    position: relative;
+    color: white;
+    background: #261e1c;
+    font-family: Prata, serif;
+    font-size: 22px;
+    text-align: left;
+    height: 35px;
+    vertical-align: center;
+    cursor: pointer;
+    
+}
+.accordion-item-title::before{
+    content: "+";
+    position: absolute;
+    top: 50%;
+    transform:translate(0, -50%);
+    right: 20px;
+    font-size: 24px;
+}
+.accordion-item-content{
+    position: relative;
+    color: #898082;
+    background: #261e1c;
+    font-family: Rubik, sans-serif;
+    font-size: 16px;
+    height: 0;
+    overflow: hidden;
+    transition: .5s ease-in-out;
+    border-bottom: 1px solid #777;
+}
+.accordion-item.active .accordion-item-content{
+    height: 35px;
+}
